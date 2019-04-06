3da463c696fe49c1c386c06c1fe2c5c2783d2bf0
visualstudio <visualstudio@122-PC22>
2019-04-05 20:29:58 -0400

    sectionA

--- 732152_assingment/Program.cs
+++ 732152_assingment/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,60 @@ namespace _732152_assingment
 {
     class Program
     {
+        ArrayList Beowulf;
+
         static void Main(string[] args)
-        { int i= 5; 
+        {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+            p.ReadTextFiles();
+
+            Console.ReadLine();
         }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+        public void ReadTextFiles()
+        {
+            // Read file using StreamReader. Read file line by line
+            using (StreamReader file = new StreamReader("U:/Users/732152/New folder/732152_assingment/732152_assingment/Beowulf.txt"))
+            {
+
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+
+                counter = File.ReadLines("U:/Users/732152/New folder/732152_assingment/732152_assingment/Beowulf.txt").Count();
+
+                Console.WriteLine("\n\n\nThe file has " + counter + " lines");
+
+
+
+            }
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
+            int countletters = 0;
+            int countSpaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+
+                if (char.IsWhiteSpace(c)) { countletters++; }
+            }
+            return countSpaces;
+
+        }
+
     }
 }
